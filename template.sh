4b00cc4874139be3b02bd4e988e79ec3f94fbb2f
yashveersaini <yashveersaini1110@gmail.com>
2026-04-15 17:15:29 +0530

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch research/analysis.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully!."
